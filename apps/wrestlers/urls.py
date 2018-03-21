6aa418bd82f8e618c218ec63943d9b8f9e8a33f2
minh <nguyenhminhp@gmail.com>
2018-03-21 16:29:08 -0700

    finished activity

--- apps/wrestlers/urls.py
+++ apps/wrestlers/urls.py
@@ -4,8 +4,8 @@ urlpatterns = [
     url(r'^wrestlers$', views.index),
     url(r'^wrestlers/new$', views.new),#html page
     url(r'^wrestlers/create$', views.create),
-    url(r'^wrestlers/(?P<wrestlers_id>\d+)', views.show),
-    url(r'^wrestlers/(?P<wrestlers_id>\d+)/edit', views.edit),
-    url(r'^wrestlers/(?P<wrestlers_id>\d+)/update', views.update),
-    url(r'^wrestlers/(?P<wrestlers_id>\d+)/delete', views.destroy),
+    url(r'^wrestlers/(?P<wrestler_id>\d+)$', views.show),
+    url(r'^wrestlers/(?P<wrestler_id>\d+)/edit$', views.edit),
+    url(r'^wrestlers/(?P<wrestler_id>\d+)/update$', views.update),
+    url(r'^wrestlers/(?P<wrestler_id>\d+)/delete$', views.destroy),
 ]
--- apps/wrestlers/views.py
+++ apps/wrestlers/views.py
@@ -3,10 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
 from .models import Wrestler
+
 # Create your views here.
 def index(req):
     context = {
-        "wrestlers": Wrestler.objects.all()
+        "wrestlers": 
     }
     return render(req, 'wrestlers/index.html', context)
 
@@ -14,29 +15,29 @@ def create(req):
     name = req.POST['name']
     weight = req.POST['weight']
     nick_name = req.POST['nick_name']
-    Wrestler.objects.create(name=name, weight=weight, nick_name=nick_name)
+    
     return redirect('/wrestlers')
 
 def edit(req, wrestler_id):
     context = {
-        "wrestler": Wrestler.objects.get(id=wrestler_id)
+        "wrestler": 
     }
     return render(req, "wrestlers/edit.html", context)
 def show(req, wrestler_id):
     context = {
-        "wrestler": Wrestler.objects.get(id=wrestler_id)
+        "wrestler": 
     }
-    return render("wrestlers/show.html", context)
+    return render(req, "wrestlers/show.html", context)
 
 def update(req, wrestler_id):
-    wrestler = Wrestler.objects.get(id=wrestler_id)
-    wrestler.name = request.form['name'],
-    wrestler.weight = request.form['weight'],
-    wrestler.nick_name = request.form['nick_name']
+    wrestler = 
+    wrestler.name =  req.POST['name']
+    wrestler.weight = req.POST['weight']
+    wrestler.nick_name = req.POST['nick_name']
     wrestler.save()
-    return redirect('/')
+    return redirect('/wrestlers')
 def new(req):
     return render(req, "wrestlers/new.html")
 def destroy(req, wrestler_id):
-    Wrestler.objects.get(id=wrestler_id).delete()
-    return redirect('/')
+    
+    return redirect('/wrestlers')
